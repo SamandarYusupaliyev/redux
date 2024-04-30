f625a55bd258473b68b6c6176cca96d18bd0758f
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-04-30 15:16:24 +0500

    papka

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { FaSun,FaMoon } from "react-icons/fa6";
-import { Link } from 'react-router-dom'
+import { useEffect, useState} from "react";
+import { Link } from "react-router-dom";
 
 
 const themes ={
@@ -36,11 +37,11 @@ function Navbar() {
             {/* sun icon */}
             <FaSun className="swap-on fill-current w-5 h-5"/>
             {/* moon icon */}
-            <FaMoon className="swap-off fill-current w-5 h-5" />
+            <FaMoon className="swap-off fill-current w-5 h-5" /> 
           </label>
          <div className="indicator items-center">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-            <span className="badge badge-sm indicator-item">0</span>
+            <span className="badge badge-sm indicator-item  ">0</span>
           </div>
        </div>
  </div>
