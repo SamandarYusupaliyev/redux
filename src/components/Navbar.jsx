2ae3352ab51816a632d2d603772d53cf5298572e
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-04-30 12:57:45 +0500

    papka

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,42 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+      <div className="bg-base-300 duration-300 transition " style={{backgroundColor:navbarBgColor}}>
+        <div className="navbar align-element">
+            <div className="navbar-start">
+                <Link to ="/" className="btn btn-primary lg:btn-lg hidden lg:flex ">MyKitchen</Link>
+              <div className="dropdown lg:hidden">
+                  <button tabIndex={0} role="button"  className="btn  btn-primary lg:btn-lg mt-3">MK</button>
+                  <div tabIndex={0} className="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52">
+                    <NavLinks/>
+                  </div>
+                </div>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+              <NavLinks/>
+            </div>
+            <div className="navbar-end">
+            {user && <p className="mr-3">{user.displayName}</p>}
+            <div className="dropdown dropdown-end">
+            <div tabIndex={0} 
+              role="button" 
+              className="btn btn-ghost btn-circle avatar">
+             <div className="w-10 rounded-full">
+                <img 
+                alt={`${user.displayName ?? "user"}image`}
+                src={user.photoURL} 
+                />
+            </div>
+            </div>
+            <ul tabIndex={0} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+             <li>
+                <button onClick={signOutFunc} className="btn btn-sm">Logout</button>
+             </li>
+           </ul>
+        </div>
+      </div>
+     </div>
+    </div>
   )
 }
 
