18914766ac04ecc5753f72f62483f0337c94a9c1
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-05-01 08:49:10 +0500

    papka

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,9 @@
 function About() {
   return (
     <div>
-      <h1>Home</h1>
+       <div>
+        <h1 className="text-3xl font-bold">We love</h1>
+       </div>
       <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
       Eos perferendis fugit assumenda, cupiditate incidunt ipsa fuga voluptates culpa deleniti dolore quam magnam et minima similique delectus voluptate veritatis. 
       Itaque hic nobis quibusdam incidunt tempore vero, accusamus, alias earum quaerat totam obcaecati pariatur. 
