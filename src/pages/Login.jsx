fe757690f1d8dd50ff6d7d708c6147e177b930f0
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-05-01 08:29:54 +0500

    papka

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,39 @@
+import { Link,Form } from "react-router-dom"
+
 function Login() {
-    return (
-      <div>
-         <h1>Login</h1>
-         <p>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. 
-          Delectus minima corporis obcaecati minus earum modi enim aperiam quasi quis eius, ex, fugiat odit dolorem sit tenetur nisi, quos architecto. 
-          Ipsa, ad quibusdam. Voluptas, ea. Necessitatibus eos ratione explicabo eaque! Rem voluptatibus enim laboriosam ratione a corporis. Odio reprehenderit distinctio veritatis!
-         </p>
-      </div>
-    )
-  }
-  
-  export default Login
+  return (
+    <div className="flex justify-center items-center mt-[100px]">
+    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100 ">
+  <form className="card-body">
+    <div className="form-control">
+      <h1 className="text-3xl font-bold text-center mb-4">Register</h1>
+      <label className="label">
+        <span className="label-text">Username</span>
+      </label>
+      <input type="email" placeholder="username" className="input input-bordered" required />
+      <label className="label">
+        <span className="label-text">Email</span>
+      </label>
+      <input type="email" placeholder="email" className="input input-bordered" required />
+    </div>
+    <div className="form-control">
+      <label className="label">
+        <span className="label-text">Password</span>
+      </label>
+      <input type="password" placeholder="password" className="input input-bordered" required />
+    </div>
+    <div className="form-control mt-6">
+      <button className="btn btn-primary mb-3">Register</button>
+        <p className="text-center">Already a member? 
+          <Link className="link link-hover  text-blue-600" to="/login">
+          Login
+        </Link>
+        </p>
+    </div>
+  </form>
+</div>
+</div>
+)
+}
+
+export default Login
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom"
+import { Link,Form } from "react-router-dom"
 
 function Register() {
   return(
