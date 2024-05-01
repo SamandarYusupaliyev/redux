b676c1574322ec2cd6bb8d9283cda29d2e1f14ce
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-05-01 08:31:32 +0500

    papka

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,11 +6,7 @@ function Login() {
     <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100 ">
   <form className="card-body">
     <div className="form-control">
-      <h1 className="text-3xl font-bold text-center mb-4">Register</h1>
-      <label className="label">
-        <span className="label-text">Username</span>
-      </label>
-      <input type="email" placeholder="username" className="input input-bordered" required />
+      <h1 className="text-3xl font-bold text-center mb-4">Login</h1>
       <label className="label">
         <span className="label-text">Email</span>
       </label>
@@ -25,8 +21,8 @@ function Login() {
     <div className="form-control mt-6">
       <button className="btn btn-primary mb-3">Register</button>
         <p className="text-center">Already a member? 
-          <Link className="link link-hover  text-blue-600" to="/login">
-          Login
+          <Link className="link link-hover  text-blue-600" to="/register">
+          Register
         </Link>
         </p>
     </div>
