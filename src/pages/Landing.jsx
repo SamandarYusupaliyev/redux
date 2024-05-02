026fec8c5c84c6f23d5cf39be7a49dcad91615f4
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-05-02 11:11:53 +0500

    papka

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -2,10 +2,9 @@ import Hero from "../components/Hero"
 
 function Landing() {
   return (
-    <div className="align-content">
+    <div className="align-content pt-10">
       <Hero/>
     </div>
   )
 }
-
 export default Landing
