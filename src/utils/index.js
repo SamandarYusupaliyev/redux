3baa0e76ada566b3e913f482c3dc2bcf0ccdf00b
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-05-03 09:57:06 +0500

    papka

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,3 +5,13 @@ const productionUrl ="https://strapi-store-server.onrender.com/api";
 export const customFetch =axios.create({
     baseURL:productionUrl,
 })
+
+
+export const formatPrice =(price)=>{
+    const dollarAmount =new Intl.NumberFormat("en-US",{
+        currency:"USD",
+        style:"currency",
+    }).format((price/100).toFixed())
+
+    return dollarAmount;
+}
