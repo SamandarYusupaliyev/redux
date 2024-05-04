7111e7f8e5986e0a549f863fe3dd658b49fde396
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-05-04 11:00:33 +0500

    papka

--- src/pages/SingleRroduct.jsx
+++ src/pages/SingleRroduct.jsx
@@ -1,16 +1,48 @@
+import { useLoaderData , Link } from "react-router-dom"
+import { customFetch, formatPrice } from "../utils"
+import { useState } from "react"
 
+export const loader = async (params) => {
+  const request = await customFetch(`/products/${params.params.id}`);
+  return {product: request.data.data};
+}
 
-function SingleRroduct() {
+function SingleProduct() {
+  const {product} = useLoaderData();
+  const {image, title, description, price, colors, company} = product.attributes;
+  const dollarAmount = formatPrice(price)
+  const [productColor , setProductColor ] = useState(colors[0])
   return (
-    <div>
-        <h1>SingleRroduct</h1>
-        <p>
-         Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-         Incidunt veniam placeat expedita minima voluptas. 
-         Officiis, repudiandae saepe voluptate earum ipsum dignissimos ratione ex similique nobis eveniet nostrum placeat itaque ipsa id provident necessitatibus laboriosam numquam commodi! Facilis, sed, odio magni eum quis labore nesciunt molestiae voluptatibus quaerat, ipsum exercitationem doloribus.
-        </p>
-    </div>
+    <section className="align-content">
+      <div className="text-md breadcrumbs">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/products">Products</Link>
+          </li>
+        </ul>
+      </div>
+
+      {/*PRODUCT*/}
+
+      <div className="mt-6 grid gap-y-8 lg:grid-cols-2 lg:gap-x-16">
+        <img className="w-96 h-96 object-cover rounded-lg" src={image} alt={title} />
+        <div>
+          <h1 className="capitalize text-3xl font-bold">{title}</h1>
+          <h4 className="title-xl font-bold text-netural-content mt-2">{company}</h4>
+          <p className="mt-3 text-xl">{dollarAmount}</p>
+          <p className="mt-6 leading-8">{description}</p>
+          <div className="mt-2">
+            {colors.map((color)=>{
+              return <button className={`badge w06 h-6 mr-2 ${color == productColor && "border-2 border-secondary"}`} style={{backgroundColor: color}} onClick={() => setProductColor(color)}></button>
+            })}
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
-export default SingleRroduct
+export default SingleProduct
