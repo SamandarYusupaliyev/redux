55a4b8f97cbdce283f1c8e1c3ac4d2cfaed48aa7
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-05-04 20:16:05 +0500

    papka

--- src/components/ProductsGrid.jsx
+++ src/components/ProductsGrid.jsx
@@ -12,11 +12,12 @@ function ProductsGrid() {
             return(
                 <Link className="card w-full shadow hover:shadow-2xl transition duration-300"
                   key={product.id}
-                  to={`/products${product.id}`}
+                  to={`/products/${product.id}`}
                 >
                  <figure className="px-4 pt-4">
                     <img className="rounded-xl h-64 md:h-48 w-full object-cover" 
-                    src={image} alt={title} />
+                    src={image} 
+                    alt={title} />
                  </figure>
                    <div className="card-body items-center text-center">
                       <h2 className="card-title capitalize tracking-wider">{title}</h2>
