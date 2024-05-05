d2182860685d71e17063caf991c31f7c1f574705
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-05-05 18:21:20 +0500

    papka

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,16 @@ function Header() {
   return (
     <header className="bg-neutral py-2 text-neutral-content">
       <div className="align-content flex justify-center sm:justify-end">
+        <div className="flex gap-x-2 sm:gap-x-8 items-center">
+          <p className="text-xs sm:text-sm"></p>
+          <button className="btn btn-xs btn-outline btn-primary"></button>
+        </div>
             {/* User */}
             {/* Links */}
-            <div className="flex gap-x-6 justify-center items-center sm:justify-end">
-                <Link to="/login" className="link link-hover text-xs sm:textsm">Sign in / Guest</Link>
-                <Link to="/register" className="link link-hover text-xs sm:textsm">Create an Account</Link>
-            </div>
+          <div className="flex gap-x-6 justify-center items-center sm:justify-end">
+              <Link to="/login" className="link link-hover text-xs sm:textsm">Sign in / Guest</Link>
+              <Link to="/register" className="link link-hover text-xs sm:textsm">Create an Account</Link>
+           </div>
       </div>
     </header>
   )
