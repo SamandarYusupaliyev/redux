e60707630907fa7ab9d10ef534892d77026f2494
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-05-06 08:23:10 +0500

    papka

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,6 @@ function Header() {
   return (
     <header className="bg-neutral py-2 text-neutral-content">
       <div className="align-content flex justify-center sm:justify-end">
-        <div className="flex gap-x-2 sm:gap-x-8 items-center">
-          <p className="text-xs sm:text-sm">"Hello ," "demo user"</p>
-          <button className="btn btn-xs btn-outline btn-primary">Logout</button>
-        </div>
             {/* User */}
             {/* Links */}
           <div className="flex gap-x-6 justify-center items-center sm:justify-end">
