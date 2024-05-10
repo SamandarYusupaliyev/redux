7f3fbb8a14692503bc364eaa9ab5ae732cfecb76
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-05-10 07:49:14 +0500

    papkA

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { Provider } from 'react-redux'
-import { Toaster } from 'react-hot-toast'
-import { store } from './store.js'
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { Provider } from "react-redux";
+import { Toaster } from "react-hot-toast";
+import { store } from "./pages/store.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <Toaster/>
+    <Toaster />
     <App />
-  </Provider>,
-)
-  
- 
+  </Provider>
+);
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,36 +1,27 @@
-import Filters from "../components/Filters";
 import ProductsContainer from "../components/ProductsContainer";
 import { customFetch } from "../utils";
+import Filters from "../components/Filters";
+import PaginationContainer from "../components/PaginationContainer";
 
-
-const url = '/products'
-
+const url = "/products";
 
 export const loader = async ({ request }) => {
   const response = await customFetch(url);
   const params = Object.fromEntries([
-    ...new URL(request.url).searchParams.entries()
+    ...new URL(request.url).searchParams.entries(),
   ]);
-  const response = await customFetch(url , {params});
+  const response = await customFetch(url, { params });
   const products = response.data.data;
   const meta = response.data.meta;
-  console.log(products , meta);
-  return { products , meta , }
+  return { products, meta };
 };
-
 function Products() {
   return (
     <div className="align-content mt-14">
-      {/* FILTER*/}
-      <Filters/>
-      {/* ProductsContainer*/}
-      <ProductsContainer/>
-
-      {/* PaginationContainer*/}
-      <PaginationContainer/>
-
+      <Filters />
+      <ProductsContainer />
+      <PaginationContainer />
     </div>
-  )
+  );
 }
-
 export default Products;
