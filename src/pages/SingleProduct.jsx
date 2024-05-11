4bd5af39bac1e26bedd24306528273c8a88c2272
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-05-11 18:56:23 +0500

    papka

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -2,7 +2,6 @@ import { useLoaderData , Link } from "react-router-dom"
 import { customFetch, formatPrice } from "../utils"
 import { useState } from "react"
 
-import { customFetch, formatPrice } from "../utils"
 import { generateAmountOptions } from "../utils"
 
 import { useDispatch } from "react-redux"
@@ -54,7 +53,7 @@ function SingleProduct() {
         <img className="w-96 h-96 object-cover rounded-lg" src={image} alt={title} />
         <div>
           <h1 className="capitalize text-3xl font-bold">{title}</h1>
-          <h4 className="title-xl font-bold text-netural-content mt-2">{company}</h4>
+          <h4 className="text-xl font-bold text-neutral-content mt-2">{company}</h4>
           <p className="mt-3 text-xl">{dollarAmount}</p>
           <p className="mt-6 leading-8">{description}</p>
           <div className="mt-2">
@@ -69,7 +68,7 @@ function SingleProduct() {
             <select className="select select-secondary select-bordered select-md" value={amount} onChange={(e) => setAmount(e.target.value)}>{generateAmountOptions(20)}</select>
           </div>
           <div className="mt-10"><button onClick={() =>console.log("add to bag")} className="btn btn-secondary btn-md">Add to bag</button></div>
-          <div className="mt-10"><button onClick={addToCart} className="btn btn-secondary btn-md">Add to bag</button></div>
+         <div className="mt-10"><button onClick={addToCart} className="btn btn-secondary btn-md">Add to bag</button></div>  
         </div>
       </div>
     </section>
