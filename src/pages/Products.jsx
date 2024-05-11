d0c3c6000b4c6e5b2da10a66d15ebceae7059f46
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-05-11 18:50:32 +0500

    papka

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,33 +1,27 @@
-import { Filters, ProductsContainer } from "../components"
-import { Filters, PaginationContainer, ProductsContainer } from "../components"
+import ProductsContainer from "../components/ProductsContainer";
 import { customFetch } from "../utils";
+import Filters from "../components/Filters";
+import PaginationContainer from "../components/PaginationContainer";
 
 const url = "/products";
 
 export const loader = async ({ request }) => {
   const response = await customFetch(url);
   const params = Object.fromEntries([
-    ...new URL(request.url).searchParams.entries()
+    ...new URL(request.url).searchParams.entries(),
   ]);
-  const response = await customFetch(url , {params});
-  const products = response.data.data;
-  const meta = response.data.meta;
-  console.log(products , meta);
-  return { products , meta , }
+  const responseData = await customFetch(url, { params });
+  const products = responseData.data.data;
+  const meta = responseData.data.meta;
+  return { products, meta };
 };
-
 function Products() {
   return (
-    <div className="align-content mt-14">
-      {/* FILTER*/}
+    <div className="align-content py-20">
       <Filters/>
-      {/* ProductsContainer*/}
-      <ProductsContainer/>
-
-      {/* PaginationContainer*/}
-      <PaginationContainer/>
-
+      <ProductsContainer />
+      <PaginationContainer />
     </div>
-  )
+  );
 }
-export default Products
+export default Products;
