6d89c77e71d46790cedf787c104c75b1f22ed18d
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-05-13 08:20:25 +0500

    papka

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,6 +9,11 @@ function Header() {
           <div className="flex gap-x-6 justify-center items-center sm:justify-end">
               <Link to="/login" className="link link-hover text-xs sm:textsm">Sign in / Guest</Link>
               <Link to="/register" className="link link-hover text-xs sm:textsm">Create an Account</Link>
+              <Link to="/" className="link link-hover text-xl sm:text-sm">
+                <button>
+                   <span className="border p-1 rounded-lg text-primary border-primary">Logout</span>
+                </button>
+              </Link>
            </div>
       </div>
     </header>
